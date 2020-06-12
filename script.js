64d004ef9ae7fe8eb2b883e2bb39793a2a7befb5
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-11 19:23:11 -0500

    finished if block, everything running correctly with cases for invalid number and no char selected

--- script.js
+++ script.js
@@ -28,64 +28,67 @@ var generateBtn = document.querySelector("#generate");
 // function that generates the password
 
   function generatePassword(userLength, userLower, userUpper, userNumber, userSpecial) {
-        // User Prompts as Variables
 
       // use a prompt to ask the user how long their password will be (between 8 and 128 characters). store this as a variable
       var userLength = prompt("Hello! How long would you like your password to be? (between 8 and 128)");
+      // store the userLength as a number
+      userLength = parseInt(userLength);
+      
+      // create an array that will store the types of characters the user wants
+      var typesArr = [];
 
+      // I think we need an if statement here. maybe a switch?
+      // check user input and generate approriate characters
       if (userLength >= 8 && userLength <= 128) {
+          // use a confirm to ask if the user wants to include lowercase characters
+          var userLower = confirm('Would you like your password to include lowercase letters?');
+          // use a confirm to ask if the user wants to include uppercase characters
+          var userUpper = confirm('Would you like your password to include uppercase letters?');
+          // use a confirm to ask if the user wants to include numeric characters
+          var userNumber = confirm('Would you like your password to include numerals?');
+          // use a confirm to ask if the user wants to include special characters
+          var userSpecial = confirm('Would you like your password to include special characters (i.e. !$/%^[&*)?');
+
+        // loop will give us the proper length
+        for (let i = 0; i < userLength; i++) {
+          // if the user wants lowercase letters, push i lowercase letters to the array
+          if (userLower) {
+            typesArr.push(getLowercase())[i];
+          };
+          // if the user wants uppercase letters, push i uppercase letters to the array
+          if (userUpper) {
+           typesArr.push(getUppercase())[i];
+          }
+          // if the user wants numbers, push i numbers to the array
+          if (userNumber) {
+           typesArr.push(getNumber())[i];
+          }
+          // if the user wants special characters, push i special characters to the array
+          if (userSpecial) {
+            typesArr.push(getSpecial())[i];
+          }
+          else if (userLower == false && userUpper == false && userNumber == false && userSpecial == false) {
+            alert('Please select at least one type of character for your password and try again')
+            return;
+          }
+        };
+        // if the userLength is too low or too high, ask the user for an appropriate number
       } else {
-        alert('Please enter a number between 8 and 128');
-        userLength;
+        alert('Please enter a number between 8 and 128 and try again');
+        return;
       };
-
-      // use a confirm to ask if the user wants to include lowercase characters
-      var userLower = confirm('Would you like your password to include lowercase letters?')
-
-      // use a confirm to ask if the user wants to include uppercase characters
-      var userUpper = confirm('Would you like your password to include uppercase letters?')
-
-      // use a confirm to ask if the user wants to include numeric characters
-      var userNumber = confirm('Would you like your password to include numerals?')
-
-      // use a confirm to ask if the user wants to include special characters
-      var userSpecial = confirm('Would you like your password to include special characters (i.e. !$/%^[&*)?')
-
-
-       // initialize password variable
-       var pw = [];
-       // generate the password based on the passed in information
-       // create an array that will store the types of characters the user wants
-        var typesArr = [];
-       // I think we need an if statement here. maybe a switch?
-       
+     
+      // set the result to a variable. the result will take a section of the appropriate lengthfrom the array 
+       var finalPw = typesArr.slice(0, userLength).join('');
+       console.log('TYPESARR: ', typesArr);
+       console.log('FINALPW: ', finalPw);
+       console.log('FINALPW LENGTH: ', finalPw.length)
+      // return the generated password based on the passed in information
+       return finalPw
          // if user wants lowercase characaters, include this in the final password.
          // if the user wants uppercase characters, include this in the final password.
          // if the user wants numeric characters, include this in the final password.
          // if the user wants special characters, include this in the in the final password.
-
-      // loop over length for each type
-      for (let i = 0; i < userLength; i++) {
-        // if the user wants all options
-        if (userLower && userUpper && userNumber && userSpecial) {
-           typesArr.push(getLowercase(), getUppercase(), getNumber(), getSpecial())[i];
-           pw = typesArr.slice(0, userLength + 1);
-           // if the user wants lower, upper and numbers
-        } else if (userLower && userUpper && userNumber) {
-
-
-          // if the user wants lower and uppercase letters
-        } else if (userLower && userUpper) {
-          // if the user wants only one option
-        } else if (userLower || userUpper || userNumber || userSpecial) {
-
-        }
-      }
-        
-      console.log(typesArr);
-      console.log(pw);
-      //add final pw to the pw var and return it
-
   };
 
 // Write password to the #password input
@@ -95,9 +98,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
