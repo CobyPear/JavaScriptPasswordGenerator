932ebea4128c2ccda56e6b05fffd33be4bb53fd2
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-09-06 14:46:26 -0500

    updated Password Generator to include a slider and toggle buttons

--- script.js
+++ script.js
@@ -1,127 +1,142 @@
 // Assignment Code
 "use strict";
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+const sliderRender = document.querySelector("#sliderValue");
+const slider = document.querySelector("#slider");
+const toggleBtn = document.querySelectorAll(".toggleBtn");
+// set initial value of slider value to default
+sliderRender.textContent = slider.value;
+
+const chars = {
+  lowercase: false,
+  uppercase: false,
+  numerals: false,
+  specialChars: false
+};
 
 // GENERATOR FUNCTIONS
 
-  // function that generates a random lowercase letter
-  function getLowercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  };
-  console.log(getLowercase())
-  // function that generates a random uppercase letter
-  function getUppercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  };
-  console.log(getUppercase())
-  // function that generates a random numeral
-  function getNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  };
-  console.log(getNumber())
-  // function that generates a random special character
-  function getSpecial() {
-    const special = '\\!@#$%^&*(){}[]=<>/",.:;`~';
-    return special[Math.floor(Math.random() * special.length)];
-  };
-  console.log(getSpecial());
+// function that generates a random lowercase letter
+function getLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+// function that generates a random uppercase letter
+function getUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+// function that generates a random numeral
+function getNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+// function that generates a random special character
+function getSpecial() {
+  const special = '\\!@#$%^&*(){}[]=<>/",.:;`~';
+  return special[Math.floor(Math.random() * special.length)];
+};
+
 
 // function that generates the password
-
 function generatePassword(userLength, userLower, userUpper, userNumber, userSpecial) {
+  // sets length of password to value of slider
+  userLength = slider.value
 
-    // use a prompt to ask the user how long their password will be (between 8 and 128 characters). store this as a variable
-    var userLength = prompt("Hello! How long would you like your password to be? (between 8 and 128)");
-    // store the userLength as a number
-    userLength = parseInt(userLength);
+  // create an array that will store the types of characters the user wants
+  const typesArr = [];
 
-    // create an array that will store the types of characters the user wants
-    var typesArr = [];
+  // I think we need an if statement here. maybe a switch?
+  // check user input and generate approriate characters
+  if (userLength >= 8 && userLength <= 128) {
+    // checkboxes determine type of characters to use in final password
+    const userLower = chars.lowercase;
+    const userUpper = chars.uppercase
+    const userNumber = chars.numerals
+    const userSpecial = chars.specialChars
 
-    // I think we need an if statement here. maybe a switch?
-    // check user input and generate approriate characters
-    if (userLength >= 8 && userLength <= 128) {
-        // use a confirm to ask if the user wants to include lowercase characters
-        // var userLower = confirm('Would you like your password to include lowercase letters?');
-
-        var userLower = $("#lowercase input:checkbox:checked").val()
-        // use a confirm to ask if the user wants to include uppercase characters
-        var userUpper = $("#uppercase input:checkbox:checked").val()
-        // use a confirm to ask if the user wants to include numeric characters
-        var userNumber = $("#numerals input:checkbox:checked").val()
-        // use a confirm to ask if the user wants to include special characters
-        var userSpecial = $("#special-chars input:checkbox:checked").val()
-
-        // if user wants lowercase characaters, include this in the final password.
-        // if the user wants uppercase characters, include this in the final password.
-        // if the user wants numeric characters, include this in the final password.
-        // if the user wants special characters, include this in the in the final password.
-      // loop will give us the proper length for the final password
-      for (let i = 0; i < userLength; i++) {
-        // if the user wants lowercase letters, push i lowercase letters to the array
-        if (userLower) {
-          typesArr.push(getLowercase())[i];
-        };
-        // if the user wants uppercase letters, push i uppercase letters to the array
-        if (userUpper) {
-          typesArr.push(getUppercase())[i];
-        }
-        // if the user wants numbers, push i numbers to the array
-        if (userNumber) {
-          typesArr.push(getNumber())[i];
-        }
-        // if the user wants special characters, push i special characters to the array
-        if (userSpecial) {
-          typesArr.push(getSpecial())[i];
-        }
-        else if (userLower == false && userUpper == false && userNumber == false && userSpecial == false) {
-
-          return alert('Please select at least one type of character for your password and try again') ;
-        }
+    // if user wants lowercase characaters, include this in the final password.
+    // if the user wants uppercase characters, include this in the final password.
+    // if the user wants numeric characters, include this in the final password.
+    // if the user wants special characters, include this in the in the final password.
+    // loop will give us the proper length for the final password
+    for (let i = 0; i < userLength; i++) {
+      // if the user wants lowercase letters, push i lowercase letters to the array
+      if (userLower) {
+        typesArr.push(getLowercase())[i];
       };
-      // if the userLength is too low or too high, ask the user for an appropriate number
-    } else {
-      alert('Please enter a number between 8 and 128 and try again');
-      return;
+      // if the user wants uppercase letters, push i uppercase letters to the array
+      if (userUpper) {
+        typesArr.push(getUppercase())[i];
+      }
+      // if the user wants numbers, push i numbers to the array
+      if (userNumber) {
+        typesArr.push(getNumber())[i];
+      }
+      // if the user wants special characters, push i special characters to the array
+      if (userSpecial) {
+        typesArr.push(getSpecial())[i];
+      }
+      else if (userLower == false && userUpper == false && userNumber == false && userSpecial == false) {
+        return alert('Please select at least one type of character for your password and try again')
+      }
     };
+    // if the userLength is too low or too high, ask the user for an appropriate number
+  } else {
+    alert('Please enter a number between 8 and 128 and try again');
+    return;
+  };
 
-    // set the result to a variable. the result will take a section of the appropriate lengthfrom the array
-      // var finalPw = typesArr.slice(0, userLength).join('');
+  // set the result to a variable. the result will take a section of the appropriate lengthfrom the array
+  // var finalPw = typesArr.slice(0, userLength).join('');
 
-        // shuffle the password to make it more random
-        //here's a function that will attempt to shuffle the array
-      function shufflePassword(arr) {
-        // switches up the indexes of the array
-        return arr.sort(() => Math.random() - 0.5);
-      };
+  // shuffle the password to make it more random
+  //here's a function that will attempt to shuffle the array
+  function shufflePassword(arr) {
+    // switches up the indexes of the array
+    return arr.sort(() => Math.random() - 0.5);
+  };
 
-      // set the result to a variable. the result will take a section of the appropriate lengthfrom the array
-      var finalPw = typesArr.slice(0, userLength);
-      // shuffle the password around
-      shufflePassword(finalPw);
-      // turn the password into a string
-      finalPw = finalPw.join('');
+  // set the result to a variable. the result will take a section of the appropriate lengthfrom the array
+  let finalPw = typesArr.slice(0, userLength);
+  // shuffle the password around
+  shufflePassword(finalPw);
+  // turn the password into a string
+  finalPw = finalPw.join('');
 
-      // debugging
-      console.log('TYPESARR: ', typesArr);
-      console.log('SHUFFLED PW: ', );
-      // what is the final pw?
-      console.log('FINALPW: ', finalPw);
-      // does the final password match userLength?
-      console.log('FINALPW LENGTH: ', finalPw.length);
+  // debugging
+  console.log('TYPESARR: ', typesArr);
+  console.log('SHUFFLED PW: ',);
+  // what is the final pw?
+  console.log('FINALPW: ', finalPw);
+  // does the final password match userLength?
+  console.log('FINALPW LENGTH: ', finalPw.length);
 
-      // return the generated password based on the passed in information
-      return finalPw;
+  // return the generated password based on the passed in information
+  return finalPw;
 };
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password || "Select at least one character type and try again";
 
 };
 
+// on change, update the value of the slider on the
+slider.addEventListener("change", (e) => {
+  sliderRender.textContent = slider.value;
+})
+
+// on click, toggle the appropriate property in chars
+for (const button of toggleBtn) {
+  button.addEventListener("click", (e)=> {
+    // flip the state of the char selected
+    chars[button.id] = !chars[button.id];
+  })
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
